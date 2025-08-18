233ede0b6125812ca2abbf35790520361df86992
Baljeet Singh <bsbaljeetsingh9999@gmail.com>
2025-08-18 19:39:20 +0530

    added hostname

--- src/services/graphql.ts
+++ src/services/graphql.ts
@@ -1,8 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 
-const HOST = process.env.HOSTNAME || "localhost";
-const PORT = process.env.PORT || "3000";
+const HOST = process.env.HOSTNAME || "localhost:3000";
 
-const gqlClient = new GraphQLClient(`http://${HOST}:${PORT}/api/graphql`);
+const gqlClient = new GraphQLClient(`http://${HOST}/api/graphql`);
 
 export default gqlClient;
